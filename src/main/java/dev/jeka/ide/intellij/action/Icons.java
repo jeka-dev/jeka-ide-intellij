8961846560d3c113f23556619c9ba0e62989b8e0
angibaudj <angibaudj>
2019-09-18 16:14:29 +0200

    Popup menu

--- src/main/java/dev/jeka/ide/intellij/action/Icons.java
+++ src/main/java/dev/jeka/ide/intellij/action/Icons.java
@@ -6,6 +6,6 @@ import javax.swing.*;
 
 public interface Icons {
 
-    Icon JEKA_GROUP_ACTION= IconLoader.getIcon("/icons/jk.png");
+    Icon JEKA_GROUP_ACTION= IconLoader.getIcon("/icons/knight-color-naked.svg");
 
 }
