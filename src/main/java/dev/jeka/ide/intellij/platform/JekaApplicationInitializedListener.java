9d24a157e1266340f3146826ba759bffc81e236f
djeang <djeangdev@yahoo.fr>
2020-01-13 23:57:00 +0100

    fix deploy

--- src/main/java/dev/jeka/ide/intellij/platform/JekaApplicationInitializedListener.java
+++ src/main/java/dev/jeka/ide/intellij/platform/JekaApplicationInitializedListener.java
@@ -1,37 +1,18 @@
-/*
- * Copyright 2018-2019 original author or authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package dev.jeka.ide.intellij.platform;
 
+import com.intellij.ide.ApplicationInitializedListener;
 import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.application.PathMacros;
-import com.intellij.openapi.components.ProjectComponent;
 
 import java.io.File;
 import java.util.Set;
 
-/**
- * @author Jerome Angibaud
- */
-public class JekaProjectComponent implements ProjectComponent {
+public class JekaApplicationInitializedListener implements ApplicationInitializedListener {
 
     private static final String JEKA_USER_HOME = "JEKA_USER_HOME";
 
     @Override
-    public void projectOpened() {
+    public void componentsInitialized() {
 
         // Instantiate jeka group action
         DefaultActionGroup jekaGroup = new ProjectPopupJekaActionGroup();
@@ -54,6 +35,6 @@ public class JekaProjectComponent implements ProjectComponent {
             String value = System.getProperty("user.home") + File.separator + ".jeka";
             pathMacros.setMacro(JEKA_USER_HOME, value);
         }
-    }
 
+    }
 }
